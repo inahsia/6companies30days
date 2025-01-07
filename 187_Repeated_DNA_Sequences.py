939de8029c0160552403c3e05f1411ad996a36e6
Aishani <141015049+inahsia@users.noreply.github.com>
2025-01-07 21:57:55 +0530

    Delete 187_Repeated_DNA_Sequences.py

--- 187_Repeated_DNA_Sequences.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        seen,result=set(),set()
-        for i in range(len(s)-9):
-            if s[i:i+10] in seen:
-                result.add(s[i:i+10])
-            seen.add(s[i:i+10])
-        return list(result)
-        
